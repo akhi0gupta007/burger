fcba5918d2268754ae79ad857b86614f3fa5c72b
Akhilesh <akhi0gupta007@gmail.com>
2020-05-14 04:09:09 +0530

    in memory outh2 server

---
+++ auth-oath2-server/src/main/java/com/akhi/app/auth/OAuth2ServerApplication.java
@@ -0,0 +1,15 @@
+package com.akhi.app.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@SpringBootApplication
+@EnableAuthorizationServer
+public class OAuth2ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2ServerApplication.class, args);
+	}
+
+}
---
+++ auth-oath2-server/src/test/java/com/akhi/app/auth/OAuth2ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akhi.app.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
