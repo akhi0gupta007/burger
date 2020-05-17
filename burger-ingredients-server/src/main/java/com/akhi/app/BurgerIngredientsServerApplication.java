953bd515cbd042d11522eb9410fce7a3063c83b7
Akhilesh <akhi0gupta007@gmail.com>
2020-05-18 00:38:03 +0530

    Method level security

--- burger-ingredients-server/src/main/java/com/akhi/app/BurgerIngredientsServerApplication.java
+++ burger-ingredients-server/src/main/java/com/akhi/app/BurgerIngredientsServerApplication.java
@@ -11,7 +11,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableDiscoveryClient
 @EnableFeignClients("com.akhi.app")
 @EnableResourceServer
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class BurgerIngredientsServerApplication {
 
 	public static void main(String[] args) {
--- burger-ingredients-server/src/main/java/com/akhi/app/resource/OrdersResource.java
+++ burger-ingredients-server/src/main/java/com/akhi/app/resource/OrdersResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,12 +21,15 @@ public class OrdersResource {
 	private OrderService orderService;
 
 	@GetMapping("/orders")
+	@PreAuthorize("hasAuthority('read_profile')")
 	public ResponseEntity<List<Orders>> getOrders(@RequestParam("userId") String userId) {
 		List<Orders> orders = orderService.getOrdersByUser(userId);
 		return new ResponseEntity<List<Orders>>(orders, HttpStatus.OK);
 	}
 	
 	@PostMapping("/orders")
+	@PreAuthorize("hasAuthority('create_profile')") //permission level check
+//	@PreAuthorize("hasRole('ROLE_editor')") // role level check 
 	public ResponseEntity<Orders> save(@RequestBody Orders orders) {
 		Orders savedOrders = orderService.save(orders);
 		return new ResponseEntity<Orders>(savedOrders, HttpStatus.CREATED);
