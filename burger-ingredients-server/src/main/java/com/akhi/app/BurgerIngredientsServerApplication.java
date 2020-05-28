c46b09d833ab9e56f1c639d32a6fceb84df17185
Akhilesh <akhi0gupta007@gmail.com>
2020-05-29 00:06:25 +0530

    Swagger API

--- burger-ingredients-server/src/main/java/com/akhi/app/BurgerIngredientsServerApplication.java
+++ burger-ingredients-server/src/main/java/com/akhi/app/BurgerIngredientsServerApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Import;
 //import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+import com.akhi.app.config.SwaggerConfig;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -16,6 +19,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @EntityScan(basePackages = "com.akhi.app.model.ingredients")
 @EnableCircuitBreaker
+@Import(SwaggerConfig.class)
 public class BurgerIngredientsServerApplication {
 
 	public static void main(String[] args) {
